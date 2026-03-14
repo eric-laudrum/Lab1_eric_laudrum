72b0a400a3d2eb63caee6568af6e57e853fce2c2
eric-laudrum <ericlaudrum@gmail.com>
2026-03-14 16:07:45 -0400

    add/ notes to outline the next steps

---
+++ Lab1_eric_laudrum/Lab1_eric_laudrum/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Lab1_eric_laudrum
+//
+//  Created by Eric Laudrum on 3/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+    
+    // Check for prime number
+    
+    
+    // Handle the user's response
+        // record the answer
+        // result displayed with green check or red x
+        
+    
+    // Handle progress update
+        // after 10 attempts, display dialogue with info on # of right and wrong answers
+    
+    
+    // Handle timer
+        // new numbers every 5 seconds
+        // no answer = X
+    
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_eric_laudrum/Lab1_eric_laudrum/Lab1_eric_laudrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_eric_laudrumApp.swift
+//  Lab1_eric_laudrum
+//
+//  Created by Eric Laudrum on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_eric_laudrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
