0c523ebe427456e74006747b5eac9b932a7a0bba
eric-laudrum <ericlaudrum@gmail.com>
2026-03-14 16:20:20 -0400

    add/ function to check for prime numbers

--- Lab1_eric_laudrum/Lab1_eric_laudrum/ContentView.swift
+++ Lab1_eric_laudrum/Lab1_eric_laudrum/ContentView.swift
@@ -19,7 +19,23 @@ struct ContentView: View {
     }
     
     // Check for prime number
-    
+    func checkIsPrime(_ number: Int) -> Bool{
+        // Prime number must have 2 distinct positive factors
+        if number <= 1{
+            return false
+        }
+        
+        // Check for factors between 1 and the number
+        for i in 2..<number{
+            // Handle factor that confirms it is a COMPOSITE NUMBER
+            if number % i == 0{
+                return false
+            }
+        }
+            
+        // If no factors are found, it is a PRIME NUMBER
+        return true
+    }
     
     // Handle the user's response
         // record the answer
